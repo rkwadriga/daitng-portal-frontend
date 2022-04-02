fc632ddcda4d153b8bca31aeaa3e72be34c2f3cf
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-04-02 13:48:53 +0300

    Add file input to "profile" page and view all loaded files to page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,6 +4,16 @@ import { ApiClient } from "../services/ApiClient";
 import { apiUrls } from "../config/api";
 import { User } from "../auth/user.entity";
 import { Notifier } from "../services/Notifier";
+import { FormControl } from "@angular/forms";
+
+interface ControlFilesNamesInterface {
+    [key: string]: boolean
+}
+interface ImageFile {
+    name: string,
+    src: string,
+    source: File
+}
 
 @Component({
   selector: 'app-profile',
@@ -12,6 +22,8 @@ import { Notifier } from "../services/Notifier";
 })
 export class ProfileComponent implements OnInit {
     user?: User;
+    files: ImageFile[] = [];
+    private filesNames: ControlFilesNamesInterface = {};
 
     constructor(
         private readonly userService: UserService,
@@ -21,6 +33,27 @@ export class ProfileComponent implements OnInit {
 
     async ngOnInit() {
         this.user = await this.userService.getUser();
+    }
+
+    onFileSelected(event: any) {
+        const file = event.target.files[0];
+        if (this.filesNames[file.name]) {
+            return;
+        }
+        this.filesNames[file.name] = true;
+
+        const self = this;
+        const reader = new FileReader();
+        reader.onloadend = function() {
+            if (typeof reader.result === 'string') {
+                self.files.push({
+                    name: file.name,
+                    src: reader.result,
+                    source: file,
+                })
+            }
+        }
+        reader.readAsDataURL(file);
     }
 
     async onSave() {
