ef97c337401cb57b1f6f49955ef1e1f90d92cc70
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-04-11 18:40:41 +0300

    Add text field for sending message to page "/pair/:id/dialog"

--- src/app/pair/dialog/dialog.component.ts
+++ src/app/pair/dialog/dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { User } from "../../services/user.service";
+import { User, UserService } from "../../services/user.service";
 import { routes } from "../../config/routes";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 export interface Message {
     from: User,
@@ -15,13 +16,23 @@ export interface Message {
   styleUrls: ['./dialog.component.scss']
 })
 export class DialogComponent implements OnInit {
+    user: User | null = null;
     @Input() pair: User | null = null;
     @Input() messages: Message[] = [];
     routes = routes;
+    msg = '';
 
-    constructor() { }
+    constructor(
+        private readonly userService: UserService
+    ) { }
 
     ngOnInit(): void {
+        this.userService.getUser().subscribe(user => {
+            this.user = user;
+        });
     }
 
+    onSend() {
+        console.log(this.msg);
+    }
 }
