a99b5ae965ed745e53c4b008a4446aa80298ca6d
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-04-24 13:30:52 +0300

    Add new photos sizes to "GET /public/img/:user_id/:path" request

--- src/app/config/photo.settings.ts
+++ src/app/config/photo.settings.ts
@@ -1,11 +1,11 @@
 
 export const photoSettings = {
-    defaultSize: '800x600',
-    avatarSize: '93x50',
-    profileSize: '500x300',
-    listSize: '300x230',
-    viewSIze: '900x750',
-    pairsListSize: '150x70',
+    defaultSize:    '800x600',
+    avatarSize:     '93x50',
+    profileSize:    '500x300',
+    listSize:       '300x230',
+    viewSIze:       '900x750',
+    pairsListSize:  '150x70',
     dialogViewSize: '400x250',
-    dialogMsgSize: '60x60',
+    dialogMsgSize:  '60x60',
 };
