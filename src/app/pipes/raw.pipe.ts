c7652f16fd8313cd9e2f3576338100a60f8bddee
rkwadriga <dmitry.kushneriov@obreey-products.com>
2022-05-04 16:12:34 +0300

    *Refactoring "raw" pipe: add limits for rows and symbols count

--- src/app/pipes/raw.pipe.ts
+++ src/app/pipes/raw.pipe.ts
@@ -7,14 +7,24 @@ import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 export class RawPipe implements PipeTransform {
     constructor(private sanitizer: DomSanitizer){}
 
-    transform(value: string | null, isXhtml = true): SafeHtml | null {
-        if (value === null) {
+    transform(htmlStr: string | null, rows = 0, length = 0, postfix = '...'): SafeHtml | null {
+        if (htmlStr === null) {
             return null;
         }
 
-        const breakTag = isXhtml ? '<br />' : '<br>';
-        return this.sanitizer.bypassSecurityTrustHtml(
-            value.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, `$1${breakTag}$2`)
-        );
+        if (length > 0 && htmlStr.length > length) {
+            htmlStr = htmlStr.substring(0, length) + postfix;
+        }
+
+        const sep = '<br />';
+        htmlStr = htmlStr.replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, `$1${sep}$2`);
+        if (rows > 0) {
+            const parts = htmlStr.split(sep);
+            if (parts.length > rows) {
+                htmlStr = parts.splice(0, rows).join(sep) + sep + postfix;
+            }
+        }
+
+        return this.sanitizer.bypassSecurityTrustHtml(htmlStr);
     }
 }
